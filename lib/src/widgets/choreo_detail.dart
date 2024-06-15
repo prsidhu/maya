1b53dc28cb5f5bc4b640ad90836e30806a1f6565
Preet Sidhu <preetsidhu.bits@gmail.com>
2024-06-14 21:15:55 -0400

    detail screen v1

--- lib/src/widgets/choreo_detail.dart
+++ lib/src/widgets/choreo_detail.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:morpheus/src/models/choreo.dart';
 import 'package:morpheus/src/providers/torch_light_controller.dart';
+import 'package:morpheus/src/utils/stringUtils.dart';
 import 'package:morpheus/src/widgets/strobo_therapy.dart';
 
 class ChoreoDetailsScreen extends ConsumerStatefulWidget {
@@ -30,23 +31,51 @@ class _ChoreoDetailsScreenState extends ConsumerState<ChoreoDetailsScreen> {
       appBar: AppBar(
         title: Text(widget.choreo.title),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Total Duration: $totalDuration seconds'),
-            Text(
-                'Media Name: ${widget.choreo.mediaName ?? 'Media unavailable'}'),
-            if (!torchLightState.isAvailable)
-              const Text('Torch light is not available on this device.'),
-            if (torchLightState.isAvailable) ...[
-              StroboTherapyWidget(
-                choreography: widget.choreo,
-              )
-            ]
-          ],
-        ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: const AssetImage(
+                    "path/to/your/fullscreen/image.jpg"), // Specify your image path
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.7), // Dark overlay
+                  BlendMode.darken,
+                ),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min, // Aligns towards the bottom
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                      padding: EdgeInsets.only(bottom: 8.0),
+                      child: Text(
+                        widget.choreo.title,
+                        style: Theme.of(context).textTheme.headlineLarge,
+                      )),
+                  Text(
+                      'Media Name: ${widget.choreo.mediaName ?? 'Media unavailable'}'),
+                  Text(
+                      'Total Duration: ${formatDuration(totalDuration)} seconds'),
+                  if (!torchLightState.isAvailable)
+                    const Text('Torch light is not available on this device.'),
+                  if (torchLightState.isAvailable) ...[
+                    StroboTherapyWidget(
+                      choreography: widget.choreo,
+                    )
+                  ]
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
