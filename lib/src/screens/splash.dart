ff4042e49b3cda3bccc25f98110cd8be36a1c2e3
Preet Sidhu <preetsidhu.bits@gmail.com>
2024-07-24 13:19:16 -0400

    final splash screen update

--- lib/src/screens/splash.dart
+++ lib/src/screens/splash.dart
@@ -1,6 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:maya/src/screens/widget_tree.dart';
-import 'package:maya/src/widgets/text/logo_text.dart';
+import 'package:maya/src/screens/home/home_page.dart';
+import 'package:maya/src/utils/auth.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -11,10 +12,29 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   double _opacity = 1.0;
+  String _errorMessage = '';
   @override
   void initState() {
     super.initState();
-    // _navigateToHome();
+    _checkAuthState();
+  }
+
+  void _checkAuthState() {
+    Auth().authStateChanges.listen((User? user) {
+      if (user != null) {
+        _navigateToHome();
+      }
+    });
+  }
+
+  Future<void> signInWithGoogle() async {
+    try {
+      await Auth().signInWithGoogle();
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        _errorMessage = e.message ?? '';
+      });
+    }
   }
 
   void _navigateToHome() async {
@@ -25,7 +45,7 @@ class _SplashScreenState extends State<SplashScreen> {
     await Future.delayed(const Duration(milliseconds: 500));
     Navigator.of(context).pushReplacement(
       MaterialPageRoute(
-        builder: (context) => const WidgetTree(),
+        builder: (context) => const HomePage(),
       ),
     );
   }
@@ -41,66 +61,69 @@ class _SplashScreenState extends State<SplashScreen> {
             child: Center(
               child: Padding(
                 padding: const EdgeInsets.all(16.0),
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Image.asset('assets/images/maya_glitter.png',
-                          width: 200.0, fit: BoxFit.cover),
-                      Padding(
-                          padding: const EdgeInsets.only(top: 16.0),
-                          child: Image.asset('assets/images/maya_text.png',
-                              width: 200.0, fit: BoxFit.cover)),
-                      Padding(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 32.0, horizontal: 24.0),
-                          child: ShaderMask(
-                            shaderCallback: (bounds) => RadialGradient(
-                              center: Alignment.center,
-                              radius: 1.0,
-                              colors: [
-                                Theme.of(context).colorScheme.primary,
-                                Theme.of(context).colorScheme.secondary,
-                              ], // Define your gradient colors here
-                              tileMode: TileMode.mirror,
-                            ).createShader(bounds),
-                            blendMode: BlendMode
-                                .srcIn, // This blend mode applies the shader to the text color
-                            child: Text(
-                              'Psychedelic light therapies for sleep & meditation',
-                              style: Theme.of(context).textTheme.headlineSmall,
-                              textAlign: TextAlign.center,
-                            ),
-                          )),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Padding(
-              padding: const EdgeInsets.only(top: 32.0, bottom: 84.0),
-              child: SizedBox(
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Add your sign-in logic here
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Theme.of(context).colorScheme.primary,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset('assets/images/maya_glitter.png',
+                        width: 200.0, fit: BoxFit.cover),
+                    Padding(
+                        padding: const EdgeInsets.only(top: 16.0),
+                        child: Image.asset('assets/images/maya_text.png',
+                            width: 200.0, fit: BoxFit.cover)),
+                    Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 32.0, horizontal: 24.0),
+                        child: ShaderMask(
+                          shaderCallback: (bounds) => RadialGradient(
+                            center: Alignment.center,
+                            radius: 1.0,
+                            colors: [
+                              Theme.of(context).colorScheme.primary,
+                              Theme.of(context).colorScheme.secondary,
+                            ], // Define your gradient colors here
+                            tileMode: TileMode.mirror,
+                          ).createShader(bounds),
+                          blendMode: BlendMode
+                              .srcIn, // This blend mode applies the shader to the text color
+                          child: Text(
+                            'Psychedelic light therapies for sleep & meditation',
+                            style: Theme.of(context).textTheme.headlineSmall,
+                            textAlign: TextAlign.center,
+                          ),
+                        )),
+                    const SizedBox(height: 84.0),
+                    ElevatedButton(
+                      onPressed: signInWithGoogle,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Theme.of(context).colorScheme.primary,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 16.0, horizontal: 48.0),
+                      ),
+                      child: Text('Sign in with Google',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyMedium
+                              ?.copyWith(
+                                color: Theme.of(context).colorScheme.onPrimary,
+                                fontWeight: FontWeight.bold,
+                              )),
                     ),
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 16.0, horizontal: 48.0),
-                  ),
-                  child: Text('Sign in with Google',
-                      style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                            color: Theme.of(context).colorScheme.onPrimary,
+                    if (_errorMessage.isNotEmpty)
+                      Padding(
+                        padding: const EdgeInsets.only(top: 16.0),
+                        child: Text(
+                          _errorMessage,
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.error,
                             fontWeight: FontWeight.bold,
-                          )),
+                          ),
+                        ),
+                      ),
+                  ],
                 ),
               ),
             ),
