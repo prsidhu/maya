5ff2050f61f2a8d1cf11aee640354aa7b644bdf3
Preet Sidhu <preetsidhu.bits@gmail.com>
2024-08-05 14:16:17 -0400

    select default goal segment

--- lib/src/providers/goal_segment_provider.dart
+++ lib/src/providers/goal_segment_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:maya/src/config/goal_segment.dart';
 
-final goalSegmentProvider = StateProvider<GoalSegment?>((ref) => null);
+final StateProvider<GoalSegment> goalSegmentProvider =
+    StateProvider<GoalSegment>((ref) => GoalSegment.five);
